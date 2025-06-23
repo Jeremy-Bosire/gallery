707f7035dfeecec26016f115e954de232f711aa0
Jeremy Bosire <bosirejeremy4@gmail.com>
2025-06-23 19:08:42 +0300

    Modifies MongoDB Connection String

--- _config.js
+++ _config.js
@@ -1,9 +1,10 @@
 var config = {}
 
 // Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+config.mongoURI = 'mongodb+srv://jeremy:mbogi26@cluster0.jmdot6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+/*{
+    production: 'mongodb+srv://<jeremy>:mbogi26@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+}*/
 module.exports = config;
