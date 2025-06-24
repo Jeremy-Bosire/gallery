bad53554b2522d4fe39b737817724b03de959008
Jeremy Bosire <bosirejeremy4@gmail.com>
2025-06-24 07:19:56 +0300

    Debugging: Slight Changes

--- _config.js
+++ _config.js
@@ -1,12 +1,9 @@
-require('dotenv').config();
-
 var config = {}
 
 // Update to have your correct username and password
-config.mongoURI = 
-{
-    production: process.env.MONGO_URI,
-    development: process.env.MONGO_DEV_URI,
-    test: process.env.MONGO_TEST_URI,
+config.mongoURI = {
+    production: 'mongodb+srv://jeremy:mbogi26@cluster0.jmdot6v.mongodb.net/gallery_prod?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://jeremy:mbogi26@cluster0.jmdot6v.mongodb.net/gallery_dev?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://jeremy:mbogi26@cluster0.jmdot6v.mongodb.net/gallery_test?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
