e5d815072ed185f8597a5b0a827ef8ea14535958
Jeremy Bosire <bosirejeremy4@gmail.com>
2025-06-24 07:14:15 +0300

    Debugging: Changes environment variables

--- _config.js
+++ _config.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 var config = {}
 
 // Update to have your correct username and password
-config.mongoURI = process.env.MONGO_URI;
-/*{
-    production: 'mongodb+srv://jeremy:mbogi26@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}*/
+config.mongoURI = 
+{
+    production: process.env.MONGO_URI,
+    development: process.env.MONGO_DEV_URI,
+    test: process.env.MONGO_TEST_URI,
+}
 module.exports = config;
