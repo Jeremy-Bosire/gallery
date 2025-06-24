2c29bdfb27a093336202c5ebf649bd74132a80ec
Jeremy Bosire <bosirejeremy4@gmail.com>
2025-06-24 06:39:58 +0300

    Degugging code

--- _config.js
+++ _config.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 var config = {}
 
 // Update to have your correct username and password
-config.mongoURI = 'mongodb+srv://jeremy:mbogi26@cluster0.jmdot6v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+config.mongoURI = process.env.MONGO_URI;
 /*{
     production: 'mongodb+srv://jeremy:mbogi26@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
